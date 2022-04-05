7eafc0685887df9518ad664780d47d26b845ee3c
Matt Wudi <wudi.matt@gmail.com>
2022-04-04 22:46:34 -0500

    Add /db-info route with table display.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-const { Pool } = requires('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -25,7 +25,34 @@ express()
         }
         catch (err) {
             console.error(err);
-            res.send("Error " + err);
+            res.send("Error: " + err);
         }
     })
-    .listen(PORT, () => console.log('Listening on ${ PORT}'));
+    .get('/db-info', async(req, res) => {
+        try {
+            const client = await pool.connect();
+            const tables = await client.query(
+                `SELECT c.relname AS table, a.attname AS column, t.typname AS type
+                FROM pg_catalog.pg_class AS c
+                LEFT JOIN pg_catalog.pg_attribute AS a
+                ON c.oid = a.attrelid AND a.attnum > 0
+                LEFT JOIN pg_catalog.pg_type AS t
+                ON a.atttypid = t.oid
+                WHERE c.relname IN ('users', 'observations', 'students', 'schools',
+                'tasks')
+                ORDER BY c.relname, a.attnum;
+                `);
+
+                const locals = {
+                    'tables': (tables) ? tables.rows : null
+                };
+
+                res.render('pages/db-info', locals);
+                client.release();
+        }
+        catch (err) {
+            console.error(err);
+            res.send("Error: " + err);
+        }
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
